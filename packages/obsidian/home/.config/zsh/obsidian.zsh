c4b8854c4923091a06a50df46cae1a258a19c3c2
Robert Roach <rjayroach@gmail.com>
2026-03-03 10:20:03 +0800

    alias to the new obsidian CLI tool

--- packages/obsidian/home/.config/zsh/obsidian.zsh
+++ packages/obsidian/home/.config/zsh/obsidian.zsh
@@ -1,5 +1,7 @@
 # obsidian
 
+alias ob="/Applications/Obsidian.app/Contents/MacOS/Obsidian"
+
 oconf() {
   local dir=$XDG_CONFIG_HOME/nvim/lua/plugins/obsidian file="../obsidian.lua" ext="lua"
   load_conf "$@"
