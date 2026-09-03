1a7cf414f76a6fe955a3263f08cb75bf5650b29d
Robert Roach <rjayroach@gmail.com>
2026-09-03 17:26:49 -0400

    alias cd to icloud dir

--- packages/obsidian/home/.config/zsh/obsidian.zsh
+++ packages/obsidian/home/.config/zsh/obsidian.zsh
@@ -1,6 +1,9 @@
 # obsidian
 
-alias ob="/Applications/Obsidian.app/Contents/MacOS/Obsidian"
+if [[ "$(os)" == "macos" ]]; then
+  alias ob="/Applications/Obsidian.app/Contents/MacOS/Obsidian"
+  alias cdi="cd $HOME/Library/Mobile\ Documents/iCloud\~md\~obsidian/Documents"
+fi
 
 oconf() {
   local dir=$XDG_CONFIG_HOME/nvim/lua/plugins/obsidian file="../obsidian.lua" ext="lua"
